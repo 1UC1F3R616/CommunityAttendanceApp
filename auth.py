7abb57643b59627561aae81ab02eb7ca22fe736a
root <kushchoudhary8@gmail.com>
2020-05-24 18:06:37 +0530

    authorization done

--- auth.py
+++ auth.py
@@ -11,30 +11,128 @@ auth_bp = Blueprint('auth_bp',
 
 from app import db
 from models import Users
+from dryFunctions import *
 
-@auth_bp.route('/user/registration', methods=['POST'])
+
+@auth_bp.route('/register/user', methods=['POST'])
 def user_registration():
-    username_ = request.json.get('username')
-    password_ = request.json.get('password')
-    print("\n\nUsername: " + username_)
-    try:
-        new_user = Users(username=username_, password=password_)
-        db.session.add(new_user)
-        db.session.commit()
+    userName = request.json.get('userName')
+    userEmail = request.json.get('userEmail')
+    userPassword = request.json.get('userPassword')
+    userPasswordConfirm = request.json.get('userPasswordConfirm')
+
+    if find_missing(userName, userEmail, userPassword, userPasswordConfirm):
+        payLoad = {
+            'userName': '',
+            'userEmail': '',
+            'message': 'Missing Params'
+        }
+
+    elif userPassword != userPasswordConfirm:
+        payLoad = {
+            'userName': '',
+            'userEmail': '',
+            'message': 'Confirmation Password Different'
+        }
+
+    elif malformed_length(
+        {
+            userName: [3, 64],
+            userEmail: [3, 64],
+            userPassword: [3, 64] 
+        }
+    ):
+        payLoad = {
+            'userName': '',
+            'userEmail': '',
+            'message': 'Param Length is Bad'
+        }
+
+    elif user_exist(email=hex_hash(userEmail)):
+        payLoad = {
+            'userName': '',
+            'userEmail': '',
+            'message': 'User Exist'
+        }
+
+    elif not is_email_valid(userEmail):
+        payLoad = {
+            'userName': '',
+            'userEmail': '',
+            'message': 'Email is not valid'
+        }
+
+    else:
+        try:
+            userEmailHash = hex_hash(userEmail)
+            userPasswordHash = hex_hash(userPassword)
+            new_user = Users(username=userName, email=userEmailHash, password=userPasswordHash)
+            db.session.add(new_user)
+            db.session.commit()
+            token = encode_auth_token(user_detail(userEmailHash).get('userId')).decode()
+            payLoad = {
+                'userName': userName,
+                'userEmail': userEmailHash,
+                'message': 'User Successfully Created',
+                'token': token
+            }
+
+            return make_response(jsonify(payLoad), 201)
+        
+        except Exception as e:
+
+            print(str(e))
+            db.session.rollback()
+            payLoad = {
+                'userName': '',
+                'userEmail': '',
+                'message': 'Something went wrong'
+            }
+    
+    return make_response(jsonify(payLoad), 400)
+    
+
+@auth_bp.route('/login/user', methods=['POST'])
+def user_login():
+    userEmail = request.json.get('userEmail')
+    userPassword = request.json.get('userPassword')
+    rememberMe = request.json.get('rememberMe')
+
+    if find_missing(userEmail, userPassword, rememberMe):
+        payLoad = {
+            'userName': '',
+            'userEmail': '',
+            'message': 'Missing Params'
+        }
+
+
+    elif not user_exist(email=hex_hash(userEmail)):
+        payLoad = {
+            'userName': '',
+            'userEmail': '',
+            'message': 'User Does not Exist'
+        }
+    
+    else:
+        userEmailHash = hex_hash(userEmail)
+        userPasswordHash = hex_hash(userPassword)
+        user_object = user_detail(userEmailHash)
+        if (userPasswordHash == user_object.get('userPassword')):
+
+            token = encode_auth_token(user_object.get('userId'), remember_me=rememberMe).decode()
+            payLoad = {
+                'userName': user_object.get('userName'),
+                'userEmail': userEmailHash,
+                'message': 'Success LogIn',
+                'token': token
+            }
+            return make_response(jsonify(payLoad), 200)
         
         payLoad = {
-            "username": username_,
-            "password": "********"
+            'userName': '',
+            'userEmail': '',
+            'message': 'Password Mismatch'
         }
-        return make_response(jsonify(payLoad), 201)
-    
-    except Exception as e:
-        print(str(e))
-        db.session.rollback()
-        payLoad = {
-            "username": None,
-            "password": "********"
-        }
-        return make_response(jsonify(payLoad), 400)
-    
+
+    return make_response(jsonify(payLoad), 400)
 
---
+++ dryFunctions.py
@@ -0,0 +1,220 @@
+"""
+Funtions to follow DRY
+"""
+
+# Imports
+import math
+import jwt
+import datetime
+import hashlib
+from app import app
+
+import re
+import smtplib
+import dns.resolver
+
+from models import Users
+
+# Distance Calculator
+def distance(origin, destination):
+
+    """
+    Find distance between two given coordinates
+    :param arg1: ['latitudeA', 'longitudeA']
+    :param arg2: ['latitudeB', 'longitudeB']
+    :type arg1: list
+    :type arg2: list
+    :return: distance in meters, round by 2
+    :type: Integer
+    """
+
+    # Conversions:
+    # d_km = d_km/1000
+    # d_miles = d_km*0.621371
+
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+    radius = 6371  # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = abs(radius * c)
+
+    return round(d*1000, 2)  # in meters
+
+
+# JWT ENCODE/DECODE FUNCTIONS
+
+def encode_auth_token(user_id, remember_me=False):
+
+    """
+    Generates the Auth Token
+    :param arg1: User ID
+    :param arg2: Increase Token Validity
+    :type arg1: Integer
+    :type arg2: Boolean
+    :return: JWT Token
+    :rtype: string
+    """
+    
+    if remember_me:
+        days_valid = 60
+    else:
+        days_valid = 1
+
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=days_valid, seconds=5), # valid upto
+            'iat': datetime.datetime.utcnow(), # created on
+            'sub': user_id
+        }
+        return jwt.encode(
+            payload,
+            'secret', #app.config.get('SECRET_KEY'), #--todo--
+            algorithm='HS256'
+        )
+    except Exception as e: # Make sure secret key is set in environment, else NoneType ValueError
+        return e
+
+
+def decode_auth_token(auth_token='auth_header'):
+
+    """
+    Decodes the auth token
+    :param arg1: JWT Token
+    :type arg1: String
+    :return: User ID | Exception
+    :rtype: Integer |  String
+    """
+    try:
+        payload = jwt.decode(auth_token, 'secret')#app.config.get('SECRET_KEY')) #--todo--
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.' # Missing SECRET_KEY will also throw this error
+
+
+def find_missing(*args):
+    """
+    If True then missing value is there
+    """
+
+
+    for value in args:
+        if value == None or value == '':
+            return True
+    else:
+        return False
+
+
+def is_email_valid(email_):
+    """
+    This takes sometime as it's verifying not only syntax
+    but if email exsists in actual or not
+    Can use a Broker like redis here but then alse we have
+    to wait for verification. Redis won't help here.
+    """
+
+    try:
+        # Address used for SMTP MAIL FROM command  
+        fromAddress = 'corn@bt.com'
+
+        # Simple Regex for syntax checking
+        regex = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$'
+
+        # Email address to verify
+        inputAddress = email_
+        addressToVerify = str(inputAddress)
+
+        # Syntax check
+        match = re.match(regex, addressToVerify)
+        if match == None:
+            return False
+
+        # Get domain for DNS lookup
+        splitAddress = addressToVerify.split('@')
+        domain = str(splitAddress[1])
+
+        # MX record lookup
+        records = dns.resolver.query(domain, 'MX')
+        mxRecord = records[0].exchange
+        mxRecord = str(mxRecord)
+
+
+        # SMTP lib setup (use debug level for full output)
+        server = smtplib.SMTP()
+        server.set_debuglevel(0)
+
+        # SMTP Conversation
+        server.connect(mxRecord)
+        server.helo(server.local_hostname) ### server.local_hostname(Get local server hostname)
+        server.mail(fromAddress)
+        code, message = server.rcpt(str(addressToVerify))
+        server.quit()
+
+
+        # Assume SMTP response 250 is success
+        if code == 250:
+            return True
+        else:
+            return False
+
+    except Exception as e:
+        print(e)
+        return False
+
+
+def malformed_length(arg1):
+    """
+    If True then length is malformed
+    Pass a Dictionary Object, key is the string
+    and Value is the tuple with min, max length inclusive
+    """
+    for key, value in arg1.items():
+        if len(key) < value[0] or len(key) > value[1]:
+            return True
+    return False
+
+
+def hex_hash(arg1):
+    """
+    return Hash of type str 
+    encoding: hex
+    algorithm: md5
+    length: 32
+    """
+
+    hash = hashlib.md5()
+    hash.update(arg1.encode())
+    return hash.hexdigest()
+
+def user_exist(email):
+    user_object = Users.query.filter_by(userEmail=email).first()
+    if user_object:
+        return True
+    return False
+
+
+def user_detail(email=None):
+
+    user_object = Users.query.filter_by(userEmail=email).first()
+    if user_object:
+        user_json = {
+            'userId': user_object.userId,
+            'userName': user_object.userId,
+            'userEmail': user_object.userEmail,
+            'userPassword': user_object.userPassword
+        }
+        return user_json
+    return {}
+
+
+def py_boolean(arg1):
+    if arg1 in ['True', 'true', True, 1]:
+        return True
+    return False
+
